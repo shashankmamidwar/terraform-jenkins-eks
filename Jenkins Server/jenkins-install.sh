1a9357db9fd645218f59202116b2e678159ec712
shashankmamidwar <47887191+shashankmamidwar@users.noreply.github.com>
2024-06-19 20:04:49 +0530

    Update jenkins-install.sh Changed to apt for ubantu previously it was yum

--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # install jenkins
-sudo yum update -y
+sudo apt update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
+sudo apt upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
+sudo apt install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
 # then install git
-sudo yum install git -y
+sudo apt install git -y
 
 #then install terraform
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+sudo apt install -y yum-utils
+sudo apt-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo apt -y install terraform
 
 #finally install kubectl
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
