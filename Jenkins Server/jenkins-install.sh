a3eed96a939c28af3982ffc5539658fb192bcf03
shashankmamidwar <47887191+shashankmamidwar@users.noreply.github.com>
2024-06-19 20:05:45 +0530

    Update jenkins-install.sh changed yo apt for ubuntu instead of yum

--- Jenkins Server/jenkins-install.sh
+++ Jenkins Server/jenkins-install.sh
@@ -2,7 +2,7 @@
 
 # install jenkins
 sudo apt update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
+sudo wget -O /etc/apt.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo apt upgrade -y
@@ -15,7 +15,7 @@ sudo systemctl start jenkins
 sudo apt install git -y
 
 #then install terraform
-sudo apt install -y yum-utils
+sudo apt install -y apt-utils
 sudo apt-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo apt -y install terraform
 
